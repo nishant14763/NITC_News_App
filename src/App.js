f39db119eed95e4644412d0e011078efdb1b5548
Nishant Kumar Bhardwaj <nishant14763@gmail.com>
2022-08-02 23:30:01 +0530

    Check point

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
 import './App.css';
-import Homepage from './components/homepage/homepage'
-import Login from "./components/login/login"
-import Forgot from "./components/forgot/forgot"
-import Registerst from "./components/registerst/registerst"
-import Registercl from "./components/registercl/registercl"
-import Logout from "./components/logout/logout"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useState } from 'react';
+
 function App() {
-  const [ user, setLoginUser] = useState({ 
-    // firstName: "",
-    // id:"1",
-    // lastName: "",
-    // emailId: "",
-    // branch: "",
-    // passingYear: "",
-    // dob: "",
-    // gender: "", 
-})
   return (
     <div className="App">
-       <Router>
-        <Switch>
-          <Route exact path="/">
-            {
-              user && user.id ?(<Homepage setLoginUser={setLoginUser} />):(<Login setLoginUser={setLoginUser}/>)
-            }
-          </Route>
-          <Route path="/login">
-            <Login setLoginUser={setLoginUser}/>
-          </Route>
-          <Route path="/registerst">
-            <Registerst />
-          </Route>
-          <Route path="/registercl">
-            <Registercl />
-          </Route>
-          <Route path="/forgot">
-            <Forgot />
-          </Route>
-          <Route path="/logout">
-            <Logout />
-          </Route>
-        </Switch>
-      </Router>
+       Hi This is Nishant !!
     </div>
   );
 }
